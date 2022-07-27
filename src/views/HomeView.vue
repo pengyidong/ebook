c29aece8f8f7d608283abd2f2f1e8ba84eecbec0
pengyidong <1124666946@qq.com>
2022-07-27 17:41:25 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <span class="icon-bookmark"></span>
+  </div>
+</template>
+
+<script>
+import Epub from "epubjs";
+global.ePub = Epub;
+export default {
+
+};
+</script>
